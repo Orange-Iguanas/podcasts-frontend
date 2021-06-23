e56260d28c5511dd2e7daf282363eba8bcb26fb4
AmandaHolliday <amandamholliday@gmail.com>
2021-06-23 11:28:42 -0500

    set up

---
+++ podcast/src/Components/episodes.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Episodes() {
+    return (
+      <div className="App">
+        <h1>Episodes</h1>
+        <ul>
+          <li>
+            
+          </li>
+          <li>
+            Episodes
+          </li>
+          <li>
+            Following
+          </li>
+        </ul>
+      </div>
+    );
+  };
+
+export default Episodes;
---
+++ podcast/src/Components/podcasts.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Podcasts() {
+    return (
+      <div className="App">
+        <h1>Podcasts</h1>
+        <div>
+        <h2>Recommended Podcasts</h2>
+        <h2>What are you looking for?</h2>
+        </div>
+      </div>
+    );
+  };
+
+export default Podcasts;
