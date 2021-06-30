0a786a907646491a548449f693c47094c5b4c259
AmandaHolliday <amandamholliday@gmail.com>
2021-06-30 13:18:14 -0500

    mess

--- podcast/src/App.js
+++ podcast/src/App.js
@@ -1,63 +1,70 @@
 import './App.css';
-// import Episodes from './Components/episodes.js';
-// import podcasts from './Components/podcasts.js';
-// import Following from './Components/following.js';
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+import { useRef } from 'react';
+import Podcasts from './Components/podcasts.js';
 import { useState, useEffect } from 'react';
-// import { Route, Link, Switch } from 'react-router-dom';
-// import InputGroup from 'react-bootstrap/InputGroup';
+import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
 import Dropdown from 'react-bootstrap/Dropdown';
-// import DropdownButton from 'react-bootstrap/DropdownButton';
-// import FormControl from 'react-bootstrap/FormControl';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 import Button from 'react-bootstrap/Button';
-// import Form from 'react-bootstrap/Form';
 import sampledata from './Models/sampledata.js';
-import Card from 'react-bootstrap/Card';
+// import Card from 'react-bootstrap/Card';
 
 function App() {
-  // const [categories, setCategories] = useState([]);
 
-  // const fetchCategories = async () => {
-  //   try {
-  //     const response = await fetch('https://www.podmyne.com/api/recommend', {
-  //       method: 'POST',
-  //       headers: {
-  //         'Content-type': 'application/json'
-  //       },
-  //       body: body
-  //     });
-  //     // setCategories(categories);
-  //     console.log("response", response);
-  //     setCategories(data);
-  //   } catch (error) {
-  //       console.log(error);
-  //   }
-  // }
-  // console.log("results", response);
+  const categorySelection = useRef(null);
+  const descriptionInput = useRef(null);
 
-  // useEffect(() => {
-  //   // fetchCategories();
-  // }, []);
+  const recommendRun = async (event) => {
+    event.preventDefault();
+    const userCat = categorySelection.current.value;
+    const userDes = descriptionInput.current.value;
+    const body = JSON.stringify({
+      userCat,userDes
+    });
+    event.currentTarget.reset();
+    console.log("testing");
+    try {
+      const response = await fetch('http://127.0.0.1:5000/app/recommend', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+          'key': 'form_data',
+        },
+        key: 'form_data',
+        body: body,
+      })
+      console.log("response", response);
+      console.log(userCat,userDes);
+      const data = await response.json();
+      console.log("data", data);
+    } catch(error) {
+      console.error(error)
+    }
+  }
 
-  // console.log(categories);
-console.log(sampledata);
+  useEffect(() => {
+
+  },[]);
 
   return (
     <div className="App">
       <h1>PodMyne</h1>
       <h2>What are your interests?</h2>
         <Dropdown>
-          {/* <DropdownButton title="Interests"> */}
+          <DropdownButton title="Interests">
             {/* have each dropdown have an onclick to call api category */}
-            {/* <Dropdown.Item>
-            hi
+            <Dropdown.Item ref={categorySelection}>
+            Food
             </Dropdown.Item>
             <Dropdown.Item>
             bye
             </Dropdown.Item>
-          </DropdownButton> */}
+          </DropdownButton>
         </Dropdown>
               <>
-        <Button variant="outline-primary">Comedy</Button>{' '}
+        <Button variant="outline-primary" ref={categorySelection}>Comedy</Button>{' '}
         <Button variant="outline-secondary">Music</Button>{' '}
         <Button variant="outline-success">Category</Button>{' '}
         <Button variant="outline-primary">Business</Button>{' '}
@@ -69,27 +76,29 @@ console.log(sampledata);
       </>
       <h2>Please enter a few sentences describing why you enjoy (category)</h2>
       <p>Example: "I love cooking and eating pizza on the weekends"</p>
-      <form className="details">
-        <input type="text"/>
-        <input type="submit"/>
+      <Router>
+        <Route path="/podcasts" component={Podcasts} />
+      </Router>
+      <form className="details" onSubmit={recommendRun}>
+        <input type="text" ref={categorySelection,descriptionInput}/>
+        <input type="submit" />
       </form>
       <div className="results">
         { sampledata 
             ?  sampledata.map((sample, index) => {
               return (
-                <div className="podcastsgroup">
-                  <Card>
-                    <div className="images">
+                <div className="cards">
+                  <ul className="cardlist">
                     <img
-                    width={200}
-                    height={200}
-                    src={sample.image}/>
-                    </div>
-                    <h5>{sample.title}</h5>
-                    <h6>{sample.categories}</h6>
-                    <div>{sample.description}</div>
-                    <a href={sample.website}>View Website</a>
-                  </Card>
+                      width={200}
+                      height={200}
+                      src={sample.image}/>
+                      <h5>{sample.title}</h5>
+                      <h5>{sample.author}</h5>
+                      <h6>{sample.categories}</h6>
+                      <div>{sample.description}</div>
+                      <a href={sample.website} target='_blank'>View Website</a>
+                  </ul>
                 {console.log(sample)}
               </div>
             )
--- podcast/src/Components/podcasts.js
+++ podcast/src/Components/podcasts.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import sampledata from '../Models/sampledata.js';
 
 function Podcasts() {
     return (
@@ -6,9 +7,28 @@ function Podcasts() {
         <h1>Podcasts</h1>
             <div>
                 <h2>Recommended Podcasts</h2>
-                <h3>Based on selected categories</h3>
-                <h2>What are you looking for?</h2>
-                <h3>Filter based on your interests</h3>
+                <div className="results">
+                { sampledata 
+                    ?  sampledata.map((sample, index) => {
+                      return (
+                        <div className="podcastsgroup">
+                            <div className="images">
+                            <img
+                            width={200}
+                            height={200}
+                            src={sample.image}/>
+                            </div>
+                            <h5>{sample.title}</h5>
+                            <h6>{sample.categories}</h6>
+                            <div>{sample.description}</div>
+                            <a href={sample.website}>View Website</a>
+                        {console.log(sample)}
+                      </div>
+                    )
+                  })
+                  : <p>Try Again</p>
+                }
+              </div>
             </div>
       </div>
     );
