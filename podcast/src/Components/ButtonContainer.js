858316eb3edaef85d58ccf5467d12fc7808cb292
Ben <Ben@Bens-MacBook-Pro.local>
2021-07-22 20:19:31 -0400

    state selection one input behind but working

--- podcast/src/Components/ButtonContainer.js
+++ podcast/src/Components/ButtonContainer.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
-import { useRef } from 'react';
+// import { useState, useEffect } from 'react';
+// import { useRef } from 'react';
 
-function ButtonContainer() {
+function ButtonContainer({ categorySelection, setCategorySelection }) {
     // const categorySelection = useRef('');
     const array = [ 'Religion & Spirituality', 'Society & Culture', 'Comedy', 'Christianity', 'TV & Film', 'Music', 'Sports & Recreation', 'News & Politics', 'Business', 'Arts', 'Education', 'Games & Hobbies', 'Health', 'Technology', 'Professional', 'Self-Help', 'Management & Marketing', 'Science & Medicine', 'Video Games', 'Kids & Family'];
 
-    useEffect(() => {
-    },[]);
+    const handleClick = (e) => {
+      console.log("value: " + e.target.value)
+      setCategorySelection(e.target.value);
+      console.log('category selection: ' + categorySelection)
+    }
     
     return (
-      <div className="App">
+      <div>
         {
-          array.map((item) => {
+          array.map((item, index) => {
             return(
-              <input type="button" value={item}
-            //   onClick={
-            //     categorySelection(item)
-            //   }
-            />
+              <input
+                key={index}
+                type="button"
+                value={item}
+                onClick={handleClick}
+              />
             );
           })
         }
